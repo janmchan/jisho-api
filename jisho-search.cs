e0258aab7b2e114754e593b960ee9a4c49ba3b0d
janmchan <jchan@barfoot.co.nz>
2023-11-03 23:26:40 +1300

    Added Jisho API call

--- jisho-search.cs
+++ jisho-search.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,29 +6,57 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace Chanjma.Function
 {
     public static class jisho_search
     {
+        
+        private static readonly HttpClient httpClient = new HttpClient();
+
         [FunctionName("jisho_search")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string keyword = req.Query["keyword"];
+
+            log.LogInformation("Jisho Search Request started. Keyword {keyword}", keyword);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             keyword = keyword ?? data?.keyword;
 
-            string responseMessage = string.IsNullOrEmpty(keyword)
-                ? "This HTTP triggered function executed successfully. Pass a keyword (&keyword=love) in the query string or in the request body for a personalized response."
-                : $"Hello, you searched for {keyword}. This HTTP triggered function executed successfully.";
+            if(string.IsNullOrEmpty(keyword))
+            {
+                return new OkObjectResult("This HTTP triggered function executed successfully. Pass a keyword (&keyword=love) in the query string or in the request body for a personalized response.");
+            }
 
-            return new OkObjectResult(responseMessage);
+            var apiUrl = string.Format("https://jisho.org/api/v1/search/words?keyword={0}", keyword);
+            try
+            {
+                // Perform the GET request
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the response content
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return new OkObjectResult(responseData);
+                }
+                else
+                {
+                    // If the request fails, return an error status code and message
+                    return new StatusCodeResult((int)response.StatusCode);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                log.LogError(ex, "Error making HTTP GET request: {url}", apiUrl);
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
